bdf11a4422a8afbf7966f2a5745b6966c2549b34
zack-murray <61975667+zack-murray@users.noreply.github.com>
2020-07-10 13:40:39 -0400

    Complete ItoP SC, including stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,13 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+        return f'{self.name}, lat coords: {self.lat}, lon coords: {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,12 +21,33 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+"""
+Usage example from csv documentation: 
+
+>>> import csv
+>>> with open('eggs.csv', newline='') as csvfile:
+...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+...     for row in spamreader:
+...         print(', '.join(row))
+Spam, Spam, Spam, Spam, Spam, Baked Beans
+Spam, Lovely Spam, Wonderful Spam
+"""
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open('cities.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    # starts loop from second row
+    next(csvfile)
+    for row in reader:
+      # city [0], lat[3], lng[4], fails if lat/lon arent float
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     
     return cities
 
@@ -59,13 +87,25 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+# input example: command = input("\nCommand: ").strip().lower().split()
+# has to be float for same reason as above, need split ',' or error: could not convert string to float
+print('\n')
+lat1, lon1 = [float(c) for c in input(f'Coordinates: lat1,lon1: ').split(',')]
+lat2, lon2 = [float(c) for c in input(f'Coordinates: lat2,lon2: ').split(',')]
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
   within = []
-
+  for c in cities:
+    if lat1 <= c.lat <= lat2 or lat1 >= c.lat >= lat2:
+      if lon1 <= c.lon <= lon2 or lon1 >= c.lon >= lon2:
+        within.append(c)
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
 
   return within
+# works! but doesn't show which cities fit the criteria
+# should be able to modify the print statement above
+for c in cityreader_stretch(lat1, lon1, lat2, lon2, cities):
+  print(c)
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle will be the base class, since flight/ground vehicles will inherit from them
+class Vehicle:
+    def __init__(self):
+        pass
+
+# Child class of vehicle, parent class of starship/airplane
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+# Child class of vehicle, parent class of car/motorcycle
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+# Child class of flightvehicle
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
+
+# Child class of flightvehicle
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+# Child class of groundvehicle
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+# Child class of groundvehicle
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
